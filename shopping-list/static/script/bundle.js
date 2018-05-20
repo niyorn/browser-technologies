a3fc3e25a5dfa647f8d8d546e129a8d14199209b
niyorn <c.niyornram@hotmail.com>
2018-05-20 12:09:37 +0200

    bundle function

--- shopping-list/static/script/bundle.js
+++ shopping-list/static/script/bundle.js
@@ -66,48 +66,47 @@
         },
         dropItem: function (ev) {
             ev.preventDefault();
-            let data = ev.dataTransfer.getData("text");
+            var data = ev.dataTransfer.getData("text");
             //Swap data 
             this.source.innerHTML = ev.target.innerHTML;
             ev.target.innerHTML = data;
         }
     };
 
-    (function () {
-        var app = {
-            init: function () {
-                //Check if the browser contain some of the feature we need to enhance it, else give the user the normal version
-                if (this.featureDectection()) {
-                    this.dragAndDrop();
-                } else {
-                    console.log('Not all feature are support');
-                }
-            },
-            featureDectection() {
-                //Check for the feature we need
-                if (featureDetection.querySelector() && featureDetection.eventListener() && featureDetection.dragAndDrop()) {
-                    return true;
-                } else {
-                    return false;
-                }
-            },
-            dragAndDrop: function () {
-                var items = document.querySelectorAll('main li');
-                items.forEach(function (i) {
-                    i.addEventListener('dragstart', (item) => {
-                        dragAndDrop.dragStart(item);
-                    });
-                    i.addEventListener('dragover', (item) => {
-                        dragAndDrop.dragOver(item);
-                    });
-                    i.addEventListener('drop', (item) => {
-                        dragAndDrop.dropItem(item);
-                    });
+    var app = {
+        init: function () {
+            //Check if the browser contain some of the feature we need to enhance it, else give the user the normal version
+            if (this.featureDectection()) {
+                this.dragAndDrop();
+            } else {
+                console.log('Not all feature are support');
+            }
+        },
+        featureDectection : function (){
+            //Check for the feature we need
+            if (featureDetection.querySelector() && featureDetection.eventListener() && featureDetection.dragAndDrop()) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        dragAndDrop: function () {
+            var items = document.querySelectorAll('main li');
+
+            for(var i = 0; i < items.length; i++){
+                items[i].addEventListener('dragstart', function (item) {
+                    dragAndDrop.dragStart(item);
+                });
+                items[i].addEventListener('dragover', function (item) {
+                    dragAndDrop.dragOver(item);
+                });
+                items[i].addEventListener('drop', function (item) {
+                    dragAndDrop.dropItem(item);
                 });
             }
-        };
-        
-        app.init();
-    })();
+        }
+    };
+
+    app.init();
 
 }());
